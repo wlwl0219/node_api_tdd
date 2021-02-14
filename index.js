1377f75caef2b39759549019fe902b4a2a8336c5
wlwl0219 <wlwl0219@gmail.com>
2021-02-15 00:53:31 +0900

    Add node http

---
+++ index.js
@@ -0,0 +1,24 @@
+const http = require("http");
+
+const hostname = "127.0.0.1";
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    res.end("Hello World\n");
+  } else if (req.url === "/users") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    res.end(JSON.stringify([{ name: "alice" }, { name: "bek" }]));
+  } else {
+    res.statusCode = 404;
+    res.setHeader("Content-Type", "text/plain");
+    res.end("Not Found");
+  }
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
